869545175cec7eb8f52683926fc9702ca15280c8
James Zow <jameszow@163.com>
2023-03-06 05:24:34 +0800

    add domain all data object

---
+++ domain/src/main/java/com/wansentech/service/package-info.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2023 wansentech.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * package wansentech.entity;
+ *
+ * This package store data object, Note: Naming specification (Bo, do)
+ *
+ * bo: Business Object, Assembly of multiple persistence layer objects.
+ * do: Domain Object, The corresponding entity of the receiving database has some mapping relationship with the database
+ *
+ * Note: Currently, the domain object has not been used, which does not mean that it will not be used later
+ */
+package com.wansentech.service;
