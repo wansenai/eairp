bdd574fbc7e9f414147078bb5335123b67a244aa
jameszow <jameszow@163.com>
2023-09-23 15:29:26 +0800

    renamed codeEnum -> BaseCodeEnum.java

--- utils/src/main/java/com/wansensoft/utils/enums/BaseCodeEnum.java
+++ utils/src/main/java/com/wansensoft/utils/enums/BaseCodeEnum.java
@@ -16,39 +16,22 @@ import lombok.Getter;
 
 /**
  * <p>
- *  响应状态枚举
+ *  通用响应状态枚举
  * </p>
  */
 @Getter
-public enum CodeEnum {
+public enum BaseCodeEnum {
 
     // 一级基本宏观状态码
     SUCCESS("00000", "系统执行成功"),
+
     ERROR("B0001", "系统执行出错"),
-    // 用户类代码
-    REGISTER_SUCCESS("A0001", "用户注册成功"),
-
-    USER_NAME_EXISTS("A0111", "用户名已存在"),
-
-    USER_LOGOUT("A0012", "账户注销成功"),
-
-    PHONE_EXISTS("A0112", "当前手机号已注册，请直接登陆"),
-
-    USERNAME_OR_PASSWORD_ERROR("A0210", "登录失败，用户名或密码错误"),
-
-    UPDATE_PASSWORD_ERROR("A0211", "系统错误，密码修改失败"),
-
-    UPDATE_PASSWORD_SUCCESS("A0013", "密码修改成功"),
-
-    USER_NOT_EXISTS("A0201", "用户账户不存在"),
 
     QUERY_DATA_EMPTY("A0404", "查询数据不存在"),
 
     PARAMETER_NULL("A0410","请求必填参数为空"),
 
-    NOT_PERMISSION("D0000", "没有权限"),
-
-    VERIFY_CODE_ERROR("A0240", "用户验证码错误"),
+    VERIFY_CODE_ERROR("A0240", "验证码错误"),
 
     VERIFY_CODE_EXPIRE("A0242", "验证码已过期"),
 
@@ -63,31 +46,16 @@ public enum CodeEnum {
     /**
      * 响应状态码
      */
-    private String code;
+    private final String code;
 
     /**
      * 响应提示
      */
-    private String msg;
+    private final String msg;
 
-    CodeEnum(String code, String msg) {
+    BaseCodeEnum(String code, String msg) {
         this.code = code;
         this.msg = msg;
     }
 
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
 }
