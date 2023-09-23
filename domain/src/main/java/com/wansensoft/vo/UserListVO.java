10410d01b1c6e8b58b4117282ab650c7d146bc4a
jameszow <jameszow@163.com>
2023-09-23 15:29:06 +0800

    fix createTime filed format show error

--- domain/src/main/java/com/wansensoft/vo/UserListVO.java
+++ domain/src/main/java/com/wansensoft/vo/UserListVO.java
@@ -1,11 +1,15 @@
 package com.wansensoft.vo;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserListVO {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING)
@@ -23,6 +27,6 @@ public class UserListVO {
 
     private Integer status;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone = "GMT+8")
-    private String createTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private LocalDateTime createTime;
 }
