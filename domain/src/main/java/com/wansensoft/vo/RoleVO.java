ed7422d7be5338ebd92c19cf3881ae767e7a5d46
jameszow <jameszow@163.com>
2023-09-24 16:03:27 +0800

    Add Role Vo

---
+++ domain/src/main/java/com/wansensoft/vo/RoleVO.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2023-2033 WanSen AI Team, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://opensource.wansenai.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package com.wansensoft.vo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class RoleVO {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private Long id;
+
+    private String roleName;
+
+    private String type;
+
+    private Integer priceLimit;
+
+    private Integer status;
+}
