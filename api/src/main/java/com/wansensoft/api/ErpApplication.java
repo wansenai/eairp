076c982f67a31d4753540ca3a16fe3fa7a147fbc
James Zow <jameszow@163.com>
2023-09-25 01:20:23 +0800

    add log ignore

--- api/src/main/java/com/wansensoft/api/ErpApplication.java
+++ api/src/main/java/com/wansensoft/api/ErpApplication.java
@@ -10,7 +10,6 @@
  * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
  * and limitations under the License.
  */
-package com.wansensoft.api;
 
 import com.wansensoft.utils.ComputerInfo;
 import org.mybatis.spring.annotation.MapperScan;
