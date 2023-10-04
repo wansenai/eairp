366809f431a44f4fff94d89db0085d96c897d199
jameszow <jameszow@163.com>
2023-10-04 22:27:51 +0800

    Add category list api

--- api/src/main/java/com/wansensoft/api/product/ProductCategoryController.kt
+++ api/src/main/java/com/wansensoft/api/product/ProductCategoryController.kt
@@ -1,10 +1,19 @@
 package com.wansensoft.api.product
 
+import com.wansensoft.service.product.ProductCategoryService
+import com.wansensoft.utils.response.Response
+import com.wansensoft.vo.ProductCategoryVO
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/product/category")
-class ProductCategoryController {
+class ProductCategoryController(private val productCategoryService: ProductCategoryService) {
+
+    @GetMapping("/list")
+    fun productCategoryList(): Response<List<ProductCategoryVO>> {
+        return productCategoryService.productCategoryList()
+    }
 
 }
