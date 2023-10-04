be7dd41f6a0ad4a121d5d61f1d186e2b4caa7166
jameszow <jameszow@163.com>
2023-10-04 14:43:57 +0800

    Renaming ktolin

--- api/src/main/java/com/wansensoft/api/system/SysMenuController.kt
+++ api/src/main/java/com/wansensoft/api/system/SysMenuController.kt
@@ -1,14 +1,14 @@
 package com.wansensoft.api.system
 
 import com.wansensoft.dto.menu.AddOrUpdateMenuDTO
-import com.wansensoft.service.system.KtSysMenuService
+import com.wansensoft.service.system.SysMenuService
 import com.wansensoft.utils.response.Response
 import org.springframework.web.bind.annotation.*
 
 
 @RestController
 @RequestMapping("/menu")
-class SysMenuController(private val menuService: KtSysMenuService) {
+class SysMenuController(private val menuService: SysMenuService) {
 
     @PostMapping("addOrUpdate")
     fun addOrUpdate(@RequestBody addOrUpdateDeptDTO: AddOrUpdateMenuDTO?): Response<String> {
