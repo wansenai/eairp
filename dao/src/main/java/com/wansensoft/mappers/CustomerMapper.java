d64c127d710eb06c98d9fd5a74a998a08614818d
jameszow <jameszow@163.com>
2023-10-14 02:32:30 +0800

    Add API for customer information module (ordinary business logic) #41

---
+++ dao/src/main/java/com/wansensoft/mappers/CustomerMapper.java
@@ -0,0 +1,8 @@
+package com.wansensoft.mappers;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wansensoft.entities.basic.Customer;
+
+public interface CustomerMapper extends BaseMapper<Customer> {
+
+}
