3d65451bfc6a8177c267bf4ca0c49acb2c8cc7b1
jameszow <jameszow@163.com>
2023-10-13 16:19:05 +0800

    Add excel utils

---
+++ utils/src/main/java/com/wansensoft/utils/excel/ExcelExport.java
@@ -0,0 +1,24 @@
+package com.wansensoft.utils.excel;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ExcelExport {
+
+    /** 字段名称 */
+    String value();
+
+    /** 导出排序先后: 数字越小越靠前（默认按Java类字段顺序导出） */
+    int sort() default 0;
+
+    /** 导出映射，格式如：0-未知;1-男;2-女 */
+    String kv() default "";
+
+    /** 导出模板示例值（有值的话，直接取该值，不做映射） */
+    String example() default "";
+
+}
---
+++ utils/src/main/java/com/wansensoft/utils/excel/ExcelImport.java
@@ -0,0 +1,25 @@
+package com.wansensoft.utils.excel;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ExcelImport {
+    /** 字段名称 */
+    String value();
+
+    /** 导出映射，格式如：0-未知;1-男;2-女 */
+    String kv() default "";
+
+    /** 是否为必填字段（默认为非必填） */
+    boolean required() default false;
+
+    /** 最大长度（默认255） */
+    int maxLength() default 255;
+
+    /** 导入唯一性验证（多个字段则取联合验证） */
+    boolean unique() default false;
+}
