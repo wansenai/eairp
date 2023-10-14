7558dae7b076ba43f778de6b101c76cd11e77dc4
jameszow <jameszow@163.com>
2023-10-14 18:17:41 +0800

    remove new package

--- dao/src/main/java/com/wansensoft/mappers/basic/SystemSupplierMapper.java
+++ dao/src/main/java/com/wansensoft/mappers/basic/SystemSupplierMapper.java
@@ -10,7 +10,7 @@
  * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
  * and limitations under the License.
  */
-package com.wansensoft.mappers;
+package com.wansensoft.mappers.basic;
 
 import com.wansensoft.entities.basic.Supplier;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
