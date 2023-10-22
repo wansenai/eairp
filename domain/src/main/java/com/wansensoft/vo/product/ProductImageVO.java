44a09ef72666ea90261a4c5ba5e794ac71cdc1aa
jameszow <jameszow@163.com>
2023-10-22 20:45:48 +0800

    Add product image data object

---
+++ domain/src/main/java/com/wansensoft/vo/product/ProductImageVO.java
@@ -0,0 +1,13 @@
+package com.wansensoft.vo.product;
+
+import lombok.Data;
+
+@Data
+public class ProductImageVO {
+
+    private String type;
+
+    private String imageName;
+
+    private String imageUrl;
+}
