cc8eff474bdff2e6b5fdef0fc5994ca2ba85dcc1
jameszow <jameszow@163.com>
2023-10-29 22:57:23 +0800

    add common api GenerateId

--- web/src/api/basic/common.ts
+++ web/src/api/basic/common.ts
@@ -7,6 +7,7 @@ enum Api {
     UploadXlsx = '/v2/common/upload/excel',
     ExportXlsx = '/v2/common/export/excel',
     UploadOss = '/v2/common/uploadOss',
+    GenerateId = '/v2/common/nextId',
 }
 
 export interface UploadFileParams {
@@ -57,5 +58,13 @@ export function uploadOss(params: UploadFileParams, mode: ErrorMessageMode = 'no
         {
             errorMessageMode: mode,
         },
+    );
+}
+
+export function generateId(type: string) {
+    return defHttp.get<BaseDataResp<number>>(
+        {
+            url: `${Api.GenerateId}/${type}`,
+        }
     );
 }
