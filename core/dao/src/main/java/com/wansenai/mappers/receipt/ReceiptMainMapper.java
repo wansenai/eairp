749d4d2b1e4ef9ebf4b2982f02697b21231105da
jameszow <jameszow@163.com>
2023-10-31 23:15:34 +0800

    Add retail related business code interface

---
+++ core/dao/src/main/java/com/wansenai/mappers/receipt/ReceiptMainMapper.java
@@ -0,0 +1,7 @@
+package com.wansenai.mappers.receipt;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wansenai.entities.receipt.ReceiptMain;
+
+public interface ReceiptMainMapper extends BaseMapper<ReceiptMain> {
+}
---
+++ core/service/src/main/java/com/wansenai/service/receipt/ReceiptSubService.java
@@ -0,0 +1,7 @@
+package com.wansenai.service.receipt;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.wansenai.entities.receipt.ReceiptSub;
+
+public interface ReceiptSubService extends IService<ReceiptSub> {
+}
