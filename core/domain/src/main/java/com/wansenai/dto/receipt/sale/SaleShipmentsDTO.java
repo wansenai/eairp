9b9ca6d0a387018cb07694931420ad9d087597b8
jameszow <jameszow@163.com>
2023-11-08 17:55:45 +0800

    Add sale shipment and refund data object

---
+++ core/domain/src/main/java/com/wansenai/dto/receipt/sale/SaleShipmentsDTO.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2023-2033 WanSen AI Team, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://opensource.wansenai.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package com.wansenai.dto.receipt.sale;
+
+import com.wansenai.bo.FileDataBO;
+import com.wansenai.bo.SalesDataBO;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+public class SaleShipmentsDTO {
+
+    private Long id;
+
+    private Long customerId;
+
+    private String receiptNumber;
+
+    private String receiptDate;
+
+    private String otherReceipt;
+
+    private BigDecimal collectOfferRate;
+
+    private BigDecimal collectOfferAmount;
+
+    private BigDecimal collectOfferLastAmount;
+
+    private BigDecimal otherAmount;
+
+    private BigDecimal thisCollectAmount;
+
+    private BigDecimal thisArrearsAmount;
+
+    private Long accountId;
+
+    private List<Long> multipleAccountAmounts;
+
+    private List<Long> multipleAccountIds;
+
+    private List<Long> operatorIds;
+
+    private List<SalesDataBO> tableData;
+
+    private List<FileDataBO> files;
+
+    private Integer status;
+
+    private String remark;
+}
---
+++ core/domain/src/main/java/com/wansenai/vo/receipt/sale/SaleShipmentsDetailVO.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2023-2033 WanSen AI Team, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://opensource.wansenai.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package com.wansenai.vo.receipt.sale;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.wansenai.bo.BigDecimalSerializerBO;
+import com.wansenai.bo.FileDataBO;
+import com.wansenai.bo.SalesDataBO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaleShipmentsDetailVO {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private Long customerId;
+
+    private String receiptNumber;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private String receiptDate;
+
+    private String otherReceipt;
+
+    @JsonSerialize(using = BigDecimalSerializerBO.class)
+    private BigDecimal collectOfferRate;
+
+    @JsonSerialize(using = BigDecimalSerializerBO.class)
+    private BigDecimal collectOfferAmount;
+
+    @JsonSerialize(using = BigDecimalSerializerBO.class)
+    private BigDecimal collectOfferLastAmount;
+
+    @JsonSerialize(using = BigDecimalSerializerBO.class)
+    private BigDecimal otherAmount;
+
+    @JsonSerialize(using = BigDecimalSerializerBO.class)
+    private BigDecimal thisCollectAmount;
+
+    @JsonSerialize(using = BigDecimalSerializerBO.class)
+    private BigDecimal thisArrearsAmount;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private Long accountId;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private List<Long> multipleAccountAmounts;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private List<Long> multipleAccountIds;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private List<Long> operatorIds;
+
+    private List<SalesDataBO> tableData;
+
+    private List<FileDataBO> files;
+
+    private Integer status;
+
+    private String remark;
+}
