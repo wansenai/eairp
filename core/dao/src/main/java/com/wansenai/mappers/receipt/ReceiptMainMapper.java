051d10646a4a96e8ccec0519259586be34896ec4
jameszow <jameszow@163.com>
2023-11-09 14:09:25 +0800

    remove receipt main and sub mapper

--- core/dao/src/main/java/com/wansenai/mappers/receipt/ReceiptMainMapper.java
+++
@@ -1,7 +0,0 @@
-package com.wansenai.mappers.receipt;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.wansenai.entities.receipt.ReceiptMain;
-
-public interface ReceiptMainMapper extends BaseMapper<ReceiptMain> {
-}
--- core/dao/src/main/java/com/wansenai/mappers/receipt/ReceiptSubMapper.java
+++
@@ -1,7 +0,0 @@
-package com.wansenai.mappers.receipt;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.wansenai.entities.receipt.ReceiptSub;
-
-public interface ReceiptSubMapper extends BaseMapper<ReceiptSub> {
-}
