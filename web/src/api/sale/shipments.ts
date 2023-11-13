5bec7de3ebc9d6ac30a7dd1e49d9c9520791da91
jameszow <jameszow@163.com>
2023-11-13 21:23:30 +0800

    add: add getLinkShipmentsDetail api

--- web/src/api/sale/shipments.ts
+++ web/src/api/sale/shipments.ts
@@ -3,7 +3,8 @@ import {BaseDataResp, BaseResp} from "@/api/model/baseModel";
 import {
     QuerySaleShipmentsReq,
     AddOrUpdateReceiptSaleShipmentsReq,
-    SaleShipmentDetailData
+    SaleShipmentDetailData,
+    LinkReceiptSaleShipmentsDetailResp
 } from "@/api/sale/model/shipmentsModel"
 
 enum API {
@@ -12,6 +13,7 @@ enum API {
     GetDetail = '/sale/shipments/detail',
     UpdateStatus = '/sale/shipments/updateStatus',
     Delete = '/sale/shipments/delete',
+    GetLinkShipmentsDetail = '/sale/shipments/getLinkShipmentDetail',
 }
 
 export function getSaleShipmentsPageList(params: QuerySaleShipmentsReq) {
@@ -52,6 +54,14 @@ export function deleteSaleShipments(ids: number[]) {
     return defHttp.put<BaseResp>(
         {
             url: `${API.Delete}/${ids}`,
+        }
+    );
+}
+
+export function getLinkShipmentsDetail(receiptNumber: string) {
+    return defHttp.get<BaseDataResp<LinkReceiptSaleShipmentsDetailResp>>(
+        {
+            url: `${API.GetLinkShipmentsDetail}/${receiptNumber}`,
         }
     );
 }
