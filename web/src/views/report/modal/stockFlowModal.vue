6aa07134a3d9be55aabd90610472e13d7211c192
jameszow <jameszow@163.com>
2023-11-16 22:16:50 +0800

    update: add stock flow views

--- web/src/views/report/modal/stockFlowModal.vue
+++ web/src/views/report/modal/stockFlowModal.vue
@@ -14,10 +14,9 @@
 import {defineComponent} from "vue";
 import {BasicTable, TableAction, useTable} from "@/components/Table";
 import {useModal} from "@/components/Modal";
-import {productStockColumns, searchProductStockSchema} from "@/views/report/report.data";
+import {searchStockFlowSchema, stockFlowColumns} from "@/views/report/report.data";
 import {Tag} from "ant-design-vue";
 import {getProductStock} from "@/api/report/report";
-import XEUtils from "xe-utils";
 
 export default defineComponent({
   name: 'productStock',
@@ -25,13 +24,13 @@ export default defineComponent({
   setup() {
     const [registerModal, {openModal}] = useModal();
     const [registerTable, { reload }] = useTable({
-      title: '商品库存报表',
+      title: '查看商品库存流水',
       api: getProductStock,
       rowKey: 'id',
-      columns: productStockColumns,
+      columns: stockFlowColumns,
       formConfig: {
         labelWidth: 110,
-        schemas: searchProductStockSchema,
+        schemas: searchStockFlowSchema,
         autoSubmitOnEnter: true,
       },
       useSearchForm: true,
@@ -39,23 +38,8 @@ export default defineComponent({
       showTableSetting: true,
       striped: true,
       showIndexColumn: false,
-      showSummary: true,
-      summaryFunc: handleSummary,
     });
 
-    function handleSummary(tableData: Recordable[]) {
-      const totalInitStock = tableData.reduce((prev, next) => prev + next.initialStock, 0);
-      const totalCurrentStock = tableData.reduce((prev, next) => prev + next.currentStock, 0);
-      const totalStockAmount = tableData.reduce((prev, next) => prev + next.stockAmount, 0);
-      return [
-        {
-          _index: '合计',
-          initialStock: totalInitStock,
-          currentStock: totalCurrentStock,
-          stockAmount: `￥${XEUtils.commafy(XEUtils.toNumber(totalStockAmount), { digits: 2 })}`
-        },
-      ];
-    }
 
     async function handleCreate() {
       openModal(true, {
