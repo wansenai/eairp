9bc586e5bd696a4941258d0b0edde368e73b836e
jameszow <jameszow@163.com>
2023-11-23 20:29:20 +0800

    fetch: complete payment receipt api code

--- core/api/src/main/java/com/wansenai/api/financial/PaymentReceiptController.java
+++ core/api/src/main/java/com/wansenai/api/financial/PaymentReceiptController.java
@@ -12,10 +12,49 @@
  */
 package com.wansenai.api.financial;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.wansenai.dto.financial.AddOrUpdatePaymentDTO;
+import com.wansenai.dto.financial.QueryPaymentDTO;
+import com.wansenai.service.financial.PaymentReceiptService;
+import com.wansenai.utils.response.Response;
+import com.wansenai.vo.financial.PaymentDetailVO;
+import com.wansenai.vo.financial.PaymentVO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/financial/payment")
 public class PaymentReceiptController {
+
+    private final PaymentReceiptService paymentReceiptService;
+
+    public PaymentReceiptController(PaymentReceiptService paymentReceiptService) {
+        this.paymentReceiptService = paymentReceiptService;
+    }
+
+    @PostMapping("addOrUpdate")
+    public Response<String> addOrUpdatePaymentReceipt(@RequestBody AddOrUpdatePaymentDTO addOrUpdatePaymentDTO) {
+        return paymentReceiptService.addOrUpdatePaymentReceipt(addOrUpdatePaymentDTO);
+    }
+
+    @PostMapping("pageList")
+    public Response<Page<PaymentVO>> getPaymentReceiptPageList(@RequestBody QueryPaymentDTO queryPaymentDTO) {
+        return paymentReceiptService.getPaymentReceiptPageList(queryPaymentDTO);
+    }
+
+    @GetMapping("getDetailById/{id}")
+    public Response<PaymentDetailVO> getPaymentReceiptDetailById(@PathVariable("id") Long id) {
+        return paymentReceiptService.getPaymentReceiptDetail(id);
+    }
+
+    @PutMapping("deleteByIds")
+    public Response<String> deletePaymentReceiptByIds(@RequestParam("ids") List<Long> ids) {
+        return paymentReceiptService.deleteBatchPaymentReceipt(ids);
+    }
+
+    @PutMapping("updateStatusByIds")
+    public Response<String> updatePaymentReceiptStatusByIds(@RequestParam("ids") List<Long> ids, @RequestParam("status") Integer status) {
+        return paymentReceiptService.updatePaymentReceiptStatus(ids, status);
+    }
 }
