444bdffeba57af6ccf89335c8598a8638e5c2194
jameszow <jameszow@163.com>
2023-11-24 14:50:03 +0800

    new: add storage receipt data object

---
+++ core/domain/src/main/java/com/wansenai/dto/warehouse/OtherStorageDTO.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2023-2033 WanSen AI Team, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://opensource.wansenai.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package com.wansenai.dto.warehouse;
+
+import com.wansenai.bo.FileDataBO;
+import com.wansenai.bo.StorageShipmentStockBO;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class OtherStorageDTO {
+
+    private Long id;
+
+    private Long supplierId;
+
+    private String receiptNumber;
+
+    private String receiptDate;
+
+    private String remark;
+
+    private Integer status;
+
+    private List<StorageShipmentStockBO> tableData;
+
+    private List<FileDataBO> files;
+}
