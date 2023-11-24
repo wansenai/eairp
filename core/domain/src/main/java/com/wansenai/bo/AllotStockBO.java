1b1aa7fdfd958e39ffdfb7c9b87635e51a97b4de
jameszow <jameszow@163.com>
2023-11-24 14:48:58 +0800

    new: add allot receipt data object

---
+++ core/domain/src/main/java/com/wansenai/bo/AllotStockBO.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2023-2033 WanSen AI Team, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://opensource.wansenai.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package com.wansenai.bo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+public class AllotStockBO {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private Long id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private Long warehouseId;
+
+    private String warehouseName;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private Long otherWarehouseId;
+
+    private String otherWarehouseName;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private Long barCode;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private Long productId;
+
+    private String productName;
+
+    private String productModel;
+
+    private String productUnit;
+
+    private String productStandard;
+
+    private Integer stock;
+
+    private Integer productNumber;
+
+    @JsonSerialize(using = BigDecimalSerializerBO.class)
+    private BigDecimal unitPrice;
+
+    @JsonSerialize(using = BigDecimalSerializerBO.class)
+    private BigDecimal amount;
+
+    private String remark;
+}
