4f403cfc22c0aa25ac8b67c44789ecd79704fed0
jameszow <jameszow@163.com>
2023-11-25 15:46:10 +0800

    new: add other shipments views #214

---
+++ web/src/views/warehouse/shipments/components/ViewOtherShipmentsModal.vue
@@ -0,0 +1,128 @@
+<template>
+  <BasicModal v-bind="$attrs" @register="registerModal" :title="getTitle" @ok="handleSubmit">
+    <div class="components-page-header-demo-responsive" style="border: 1px solid rgb(235, 237, 240)">
+      <a-page-header
+          title="其他出库-详情"
+          :sub-title= "receiptNumber">
+        <template #extra>
+          <a-button key="1">导出</a-button>
+          <a-button key="1">普通打印</a-button>
+          <a-button key="1">三联打印</a-button>
+          <a-button key="2" type="primary">发起流程审批</a-button>
+        </template>
+        <a-descriptions size="small" :column="3">
+          <a-descriptions-item label="客户">{{ customerName }}</a-descriptions-item>
+          <a-descriptions-item label="单据日期">{{ receiptDate }}</a-descriptions-item>
+          <a-descriptions-item ></a-descriptions-item>
+          <a-descriptions-item label="备注">
+            {{ remark }}
+          </a-descriptions-item>
+        </a-descriptions>
+        <div class="extra">
+          <div
+              class="descriptions-context"
+              :style="{
+              display: 'flex',
+              width: 'max-content',
+              justifyContent: 'flex-end',
+            }"
+          >
+            <a-statistic
+                title="单据状态"
+                :value="status === 1 ? '已审核' : '未审核'"
+                :value-style="status === 1 ? { color: '#3f8600' } : { color: '#cf1322' }"
+                :style="{
+                marginRight: '32px',
+                color: 'green',
+              }"
+            />
+          </div>
+        </div>
+      </a-page-header>
+      <BasicTable @register="registerTable">
+      </BasicTable>
+    </div>
+  </BasicModal>
+</template>
+<script lang="ts">
+import {defineComponent, ref} from 'vue';
+import {BasicTable, useTable} from '/src/components/Table';
+import {BasicModal, useModal, useModalInner} from "@/components/Modal";
+import {getOtherShipmentsDetailById} from "@/api/warehouse/shipments";
+import {otherShipmentTableColumns} from "@/views/warehouse/shipments/otherShipments.data";
+import {
+  Descriptions,
+  DescriptionsItem,
+  PageHeader,
+  Statistic,
+} from 'ant-design-vue';
+
+export default defineComponent({
+  name: 'ViewOtherStorageModal',
+  components: {
+    BasicModal,
+    BasicTable,
+    'a-page-header': PageHeader,
+    'a-descriptions': Descriptions,
+    'a-descriptions-item': DescriptionsItem,
+    'a-statistic': Statistic,
+  },
+  setup() {
+    const receiptNumber = ref('');
+    const otherReceipt = ref('');
+    const receiptDate = ref('');
+    const customerName = ref('');
+    const remark = ref('')
+    const status = ref();
+    const [viewOrderReceiptModal, {openModal: openViewOrderModal}] = useModal();
+    const tableData = ref([]);
+    const [registerTable] = useTable({
+      title: '其他出库表数据',
+      columns: otherShipmentTableColumns,
+      dataSource: tableData,
+      pagination: false,
+      showIndexColumn: false,
+      bordered: true,
+    });
+    const getTitle = ref('单据详情');
+    const [registerModal, {setModalProps, closeModal}] = useModalInner(async (data) => {
+      setModalProps({confirmLoading: false, destroyOnClose: true, width: 1200, showOkBtn: false});
+      const res = await getOtherShipmentsDetailById(data.id);
+      tableData.value = res.data.tableData;
+      receiptNumber.value = res.data.receiptNumber;
+      customerName.value = res.data.customerName;
+      receiptDate.value = res.data.receiptDate;
+      remark.value = res.data.remark;
+      status.value = res.data.status;
+    });
+
+    function viewOrderReceipt() {
+      closeModal();
+      openViewOrderModal(true, {
+        receiptNumber: otherReceipt.value
+      });
+    }
+
+    function handleSubmit() {
+      closeModal();
+    }
+
+    return {
+      receiptNumber,
+      customerName,
+      receiptDate,
+      remark,
+      status,
+      registerTable,
+      registerModal,
+      getTitle,
+      handleSubmit,
+      viewOrderReceiptModal,
+      viewOrderReceipt
+    };
+  },
+});
+</script>
+<style scoped>
+
+</style>
