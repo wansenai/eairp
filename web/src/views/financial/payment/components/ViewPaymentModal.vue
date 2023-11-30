449e35f84c1d98b356a4b8e5bb830bcd0cd4f52e
jameszow <jameszow@163.com>
2023-11-30 17:24:24 +0800

    fetch: add printing function for financial module #244

--- web/src/views/financial/payment/components/ViewPaymentModal.vue
+++ web/src/views/financial/payment/components/ViewPaymentModal.vue
@@ -5,7 +5,10 @@
           title="付款单-详情"
           :sub-title= "receiptNumber">
         <template #extra>
-          <a-button key="1" type="primary">打印</a-button>
+          <a-button @click="exportTable">导出</a-button>
+          <a-button @click="primaryPrint" type="primary">普通打印</a-button>
+          <!--          <a-button key="triplePrint">三联打印</a-button>-->
+          <!--          <a-button key="2" type="primary">发起流程审批</a-button>-->
         </template>
         <a-descriptions size="small" :column="3">
           <a-descriptions-item label="供应商">{{ supplierName }}</a-descriptions-item>
@@ -61,6 +64,7 @@ import {
   Statistic,
 } from 'ant-design-vue';
 import {paymentReceiptTableColumns} from "@/views/financial/payment/payment.data";
+import printJS from "print-js";
 export default defineComponent({
   name: 'ViewExpenseModal',
   components: {
@@ -113,6 +117,46 @@ export default defineComponent({
       closeModal();
     }
 
+    function exportTable() {
+
+    }
+
+    const flexContainer = 'display: flex; justify-content: space-between; border-bottom: 1px solid #ddd; padding: 8px;';
+    const flexItem = 'display: flex; flex-direction: column; justify-content: space-between; font-size: 12px;';
+    function primaryPrint() {
+      const header = `
+  <div style="${flexContainer}">
+    <div style="${flexItem}">单据编号：${receiptNumber.value}</div>
+    <div style="${flexItem}">单据日期：${receiptDate.value}</div>
+    <div style="${flexItem}">单据金额：${totalPaymentAmount.value}</div>
+  </div>
+  <div style="${flexContainer}">
+    <div style="${flexItem}">付款账户：${paymentAccountName.value}</div>
+    <div style="${flexItem}">优惠金额：${discountAmount.value}</div>
+    <div style="${flexItem}">实际付款：${actualPaymentAmount.value}</div>
+  </div>
+  <div style="${flexContainer}">
+    <div style="${flexItem}">供应商：${supplierName.value}</div>
+    <div style="${flexItem}">财务人员：${financialPersonName.value}</div>
+    <div style="${flexItem}">备注：${remark.value}</div>
+  </div>
+`;
+      printJS({
+        documentTitle: "EAIRP (付款单单据-详情)",
+        header: header,
+        properties: paymentReceiptTableColumns.map((item) => {
+          return {
+            field: item.dataIndex,
+            displayName: item.title,
+          };
+        }),
+        printable: tableData.value,
+        gridHeaderStyle: 'border: 1px solid #ddd; font-size: 12px; text-align: center; padding: 8px;',
+        gridStyle: 'border: 1px solid #ddd; font-size: 12px; text-align: center; padding: 8px;',
+        type: 'json',
+      });
+    }
+
     return {
       receiptNumber,
       supplierName,
@@ -128,6 +172,8 @@ export default defineComponent({
       registerModal,
       getTitle,
       handleSubmit,
+      exportTable,
+      primaryPrint
     };
   },
 });
