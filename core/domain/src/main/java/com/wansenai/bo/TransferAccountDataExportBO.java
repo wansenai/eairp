1ea74216a18dafbd18b7a206a92ff93aae53441f
jameszow <jameszow@163.com>
2023-12-03 17:41:02 +0800

    new: add transfer export data object

---
+++ core/domain/src/main/java/com/wansenai/bo/TransferAccountDataExportBO.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2023-2033 WanSen AI Team, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://opensource.wansenai.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package com.wansenai.bo;
+
+import com.wansenai.utils.excel.ExcelExport;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+public class TransferAccountDataExportBO {
+
+    @ExcelExport(value = "单据编号")
+    private String receiptNumber;
+
+    @ExcelExport(value = "账户名称")
+    private String accountName;
+
+    @ExcelExport(value = "转账金额")
+    private BigDecimal transferAmount;
+
+    @ExcelExport(value = "备注")
+    private String remark;
+}
