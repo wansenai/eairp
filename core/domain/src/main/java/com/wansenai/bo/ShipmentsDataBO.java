8ab8e96c7417559da07881d25670811a66fa74bd
jameszow <jameszow@163.com>
2023-12-03 15:45:00 +0800

    fetch: change other sale and shipment and storage export data object

--- core/domain/src/main/java/com/wansenai/bo/ShipmentsDataBO.java
+++ core/domain/src/main/java/com/wansenai/bo/ShipmentsDataBO.java
@@ -1,8 +1,19 @@
+/*
+ * Copyright 2023-2033 WanSen AI Team, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://opensource.wansenai.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
 package com.wansenai.bo;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.wansenai.utils.excel.ExcelExport;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,46 +30,34 @@ public class ShipmentsDataBO {
     @JsonFormat(shape = JsonFormat.Shape.STRING)
     private Long warehouseId;
 
-    @ExcelExport(value = "仓库名称")
     private String warehouseName;
 
     private String productCode;
 
-    @ExcelExport(value = "条码")
     private String barCode;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING)
     private Long productId;
 
-    @ExcelExport(value = "商品名称")
     private String productName;
 
-    @ExcelExport(value = "规格")
     private String productStandard;
 
-    @ExcelExport(value = "型号")
     private String productModel;
 
-    @ExcelExport(value = "颜色")
     private String productColor;
 
-    @ExcelExport(value = "库存")
     private Integer stock;
 
-    @ExcelExport(value = "单位")
     private String productUnit;
 
-    @ExcelExport(value = "数量")
     private Integer productNumber;
 
     @JsonSerialize(using = BigDecimalSerializerBO.class)
-    @ExcelExport(value = "单价")
     private BigDecimal unitPrice;
 
     @JsonSerialize(using = BigDecimalSerializerBO.class)
-    @ExcelExport(value = "金额")
     private BigDecimal amount;
 
-    @ExcelExport(value = "备注")
     private String remark;
 }
