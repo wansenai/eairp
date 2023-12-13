a6005b28aeb35421a7900e2c9b28a7a657332406
jameszow <jameszow@163.com>
2023-12-13 18:52:35 +0800

    fetch: update account login and register logic code added password encryption RSA transmission #285

--- web/src/views/sys/login/useLogin.ts
+++ web/src/views/sys/login/useLogin.ts
@@ -2,6 +2,7 @@ import type {ValidationRule, FormInstance} from 'ant-design-vue/lib/form/Form';
 import type {RuleObject, NamePath} from 'ant-design-vue/lib/form/interface';
 import {ref, computed, unref, Ref} from 'vue';
 import {useI18n} from '/@/hooks/web/useI18n';
+import CryptoJS from 'crypto-js';
 
 export enum LoginStateEnum {
     LOGIN,
@@ -9,6 +10,37 @@ export enum LoginStateEnum {
     RESET_PASSWORD,
     MOBILE,
     QR_CODE,
+}
+
+/**
+ * AES加密
+ * @param plainText 明文
+ * @param keyInBase64Str base64编码后的key
+ * @returns {string} base64编码后的密文
+ */
+export function encryptByAES(plainText, keyInBase64Str) {
+    let key = CryptoJS.enc.Base64.parse(keyInBase64Str);
+    let encrypted = CryptoJS.AES.encrypt(plainText, key, {
+        mode: CryptoJS.mode.ECB,
+        padding: CryptoJS.pad.Pkcs7,
+    });
+    return encrypted.ciphertext.toString(CryptoJS.enc.Base64);
+}
+
+/**
+ * AES解密
+ * @param cipherText 密文
+ * @param keyInBase64Str base64编码后的key
+ * @return 明文
+ */
+export function decryptByAES(cipherText, keyInBase64Str) {
+    let key = CryptoJS.enc.Base64.parse(keyInBase64Str);
+    let decrypted = CryptoJS.AES.decrypt(cipherText, key, {
+        mode: CryptoJS.mode.ECB,
+        padding: CryptoJS.pad.Pkcs7,
+    });
+
+    return decrypted.toString(CryptoJS.enc.Utf8);
 }
 
 const currentState = ref(LoginStateEnum.LOGIN);
