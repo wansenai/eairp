14e3bb8395248c05b1f154919b4e158a68123053
Jz1ow <zhaowei@ecloudriver.com>
2024-03-28 17:45:11 +0800

    Add english support for reports modules

---
+++ web/src/locales/lang/en/reports.ts
@@ -0,0 +1,34 @@
+export default {
+    productStock: {
+        title: 'Product Stock Report',
+        export: 'Export',
+        regularPrint: 'Print',
+        data: 'Product Stock Data',
+        detail: 'product Stock Detail',
+        header: {
+            warehouse: 'Warehouse',
+            productInfo: 'Product Info',
+            productCategory: 'Product Category',
+            warehouseShelves: 'Warehouse Shelves',
+        },
+        table: {
+            flow: 'Flow',
+            stockFlow: 'Stock Flow',
+            productBarcode: 'Product Barcode',
+            warehouse: 'Warehouse',
+            productName: 'Name',
+            productCategory: 'Category',
+            standard: 'Standard',
+            model: 'Model',
+            weight: 'Weight',
+            color: 'Color',
+            unit: 'Unit',
+            warehouseShelves: 'Shelves',
+            unitPrice: 'Unit Price',
+            initStock: 'Initial Stock',
+            currentStock: 'Current Stock',
+            stockAmount: 'Stock Amount',
+            notice: 'There is no data to export under the current query conditions',
+        }
+    }
+}
---
+++ web/src/locales/lang/zh-CN/reports.ts
@@ -0,0 +1,35 @@
+export default {
+    productStock: {
+        title: '商品库存报表',
+        export: '导出',
+        regularPrint: '普通打印',
+        data: '商品库存数据',
+        detail: '商品库存明细',
+        header: {
+            warehouse: '仓库',
+            productInfo: '商品信息',
+            productCategory: '商品分类',
+            warehouseShelves: '仓库货架',
+        },
+        table: {
+            flow: '流水',
+            stockFlow: '库存流水',
+            productBarcode: '商品条码',
+            warehouse: '仓库',
+            productName: '商品名称',
+            productCategory: '商品分类',
+            standard: '规格',
+            model: '型号',
+            weight: '基础重量',
+            color: '颜色',
+            unit: '单位',
+            warehouseShelves: '仓库货架',
+            unitPrice: '单价',
+            initStock: '初始库存',
+            currentStock: '当前库存',
+            stockAmount: '库存金额',
+            total: '合计',
+            notice: '当前查询条件下无数据可导出',
+        }
+    }
+}
